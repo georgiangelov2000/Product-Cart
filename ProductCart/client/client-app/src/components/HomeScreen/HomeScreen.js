b06f548f1762f6497a92d749e671847b8a30911f
SoftAcademy/Plovdiv University/Georgi Angelov <60781748+georgiangelov2000@users.noreply.github.com>
2021-06-26 21:33:48 +0300

    added components

---
+++ ProductCart/client/client-app/src/components/HomeScreen/HomeScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomeScreen = () => {
+    return (
+        <div>
+            <h1>Prodcts</h1>
+        </div>
+    )
+}
+
+export default HomeScreen
--- ProductCart/server/server.js
+++ ProductCart/server/server.js
@@ -3,6 +3,7 @@ const app = express();
 const dotenv=require('dotenv');
 const mongoose = require('mongoose');
 
+const productsRoute=require("./routes/products");
 
 dotenv.config({path:'./config.env'})
 
@@ -15,6 +16,14 @@ mongoose.connect(process.env.DB_LOCAL,{
     useUnifiedTopology: true,
     useFindAndModify:true,
 });
+
+app.get("/api/products",productsRoute);;
+
+app.use((req,res,next)=>{
+    const error=new Error(`Not found - ${req.originalUrl}`)
+    res.status(404);
+    next(error);
+})
 
 const PORT = process.env.PORT || 5000;
 
